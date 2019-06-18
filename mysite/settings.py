e53ef51b3a0bc992235f80941463a50368a9b868
SonnySkyez <hnic85@gmail.com>
2019-06-18 11:00:47 +0200

    Collect static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'secretkeyyyy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default = False, cast=bool)
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['polls-application.herokuapp.com', 'localhost:8000', '127.0.0.1', 'sonny-skyez.herokuapp.com']
